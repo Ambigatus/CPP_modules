860036a1bb2d7240199cd758f74e9a37f7d96f84
ddzuba <ddziuba.wolfsburg@gmail.com>
2023-06-13 09:26:56 +0200

    base for new task

---
+++ CPP_02/ex01/Fixed.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddzuba <ddzuba@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 09:24:55 by ddzuba            #+#    #+#             */
+/*   Updated: 2023/06/13 09:26:10 by ddzuba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//it's old variant, fix it
+#include "Fixed.hpp"
+
+Fixed::Fixed() {
+    std::cout << "Default constructor called" << std::endl;
+    this->_numFixedPointValue = 0;
+}
+
+// The reason for calling setRawBits in the copy constructor is to ensure 
+// that the newly created object has the same raw bits value as the object being copied. 
+// It allows for a proper copy of the internal state of the object.
+Fixed::Fixed( const Fixed &rhs ) {
+    std::cout << "Copy constructor called" << std::endl;
+    this->setRawBits(rhs.getRawBits());
+    // *this = rhs; - another way for setting the value
+	//but with sRB it's more controled
+}
+
+// Assignment operator overload: Updates the state of an already initialized object with 
+// the values of another object. It is used when you want to assign the values of one object 
+// to another object that already exists, allowing you to reuse and update an object
+Fixed& Fixed::operator=( const Fixed &rhs ) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &rhs)
+        this->_numFixedPointValue = rhs.getRawBits();
+    return *this;
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+// provide access to the raw representation of the 
+// fixed-point number stored in the Fixed class.
+//  By returning the raw bits, it allows users of the class 
+//  to inspect and manipulate the internal representation if needed.
+int Fixed::getRawBits( void ) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_numFixedPointValue;
+}
+
+// provide a way to set the raw value of the fixed-point number stored in the Fixed class. 
+// It allows users of the class to initialize or modify the internal 
+// representation of the fixed-point number directly.
+void    Fixed::setRawBits( int const raw ) {
+    this->_numFixedPointValue = raw;
+}
---
+++ CPP_02/ex01/Fixed.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddzuba <ddzuba@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 09:24:44 by ddzuba            #+#    #+#             */
+/*   Updated: 2023/06/13 09:25:28 by ddzuba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//it's old variant, fix it
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+private:
+//int to store the fixed-point number value
+    int                 _numFixedPointValue;
+    
+//static const int to store the num of fractional bits
+    static const int    _numfractionalBits = 8;
+
+public:
+//default constructor(classicaly without args)
+    Fixed(void);
+    
+//copy constructor for creating a copy of obj
+    Fixed( const Fixed &rhs );
+    
+//Assignment operator overloading allows you to define your own 
+//implementation of the assignment operator for a class
+    Fixed& operator=( const Fixed &rhs );
+    
+// destructor
+    ~Fixed();
+    
+//returns the raw value of the fixed-point value
+    int     getRawBits( void ) const;
+    
+//sets the raw value of the fixed-point number
+    void    setRawBits( int const raw );
+};
+
+#endif 
