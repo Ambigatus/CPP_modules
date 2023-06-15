d81101b4fc30a608263c625451a247216b7dc764
ddzuba <ddziuba.wolfsburg@gmail.com>
2023-06-15 18:58:02 +0200

    finish second

--- CPP_02/ex02/Fixed.cpp
+++ CPP_02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: ddzuba <ddzuba@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 09:24:55 by ddzuba            #+#    #+#             */
-/*   Updated: 2023/06/14 20:27:57 by ddzuba           ###   ########.fr       */
+/*   Updated: 2023/06/15 18:55:48 by ddzuba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,14 @@
 #include <cmath> //for roundf()
 
 Fixed::Fixed(void) {
-    std::cout << "Default constructor called" << std::endl;
     this->_numFixedPointValue = 0;
 }
 
 Fixed::Fixed(const int i) {
-    std::cout << "Int constructor called" << std::endl;
     this->_numFixedPointValue = i << _numFractionalBits;
 }
 
 Fixed::Fixed(const float i) {
-    std::cout << "Float constructor called" << std::endl;
-    
     //converts floating num in fixed-point num
     this->_numFixedPointValue = roundf( i * ( 1 << _numFractionalBits ));
 }
@@ -34,32 +30,26 @@ Fixed::Fixed(const float i) {
 // that the newly created object has the same raw bits value as the object being copied. 
 // It allows for a proper copy of the internal state of the object.
 Fixed::Fixed( const Fixed &rhs ) {
-    std::cout << "Copy constructor called" << std::endl;
     this->setRawBits(rhs.getRawBits());
-    // *this = rhs; - another way for setting the value
-	//but with sRB it's more controled
+	// with sRB it's more controled
 }
 
 // Assignment operator overload: Updates the state of an already initialized object with 
 // the values of another object. It is used when you want to assign the values of one object 
 // to another object that already exists, allowing you to reuse and update an object
 Fixed& Fixed::operator=( const Fixed &rhs ) {
-    std::cout << "Copy assignment operator called" << std::endl;
     if (this != &rhs)
         this->_numFixedPointValue = rhs.getRawBits();
     return *this;
 }
 
-Fixed::~Fixed() {
-    std::cout << "Destructor called" << std::endl;
-}
+Fixed::~Fixed() {} //destructor
 
 // provide access to the raw representation of the 
 // fixed-point number stored in the Fixed class.
 //  By returning the raw bits, it allows users of the class 
 //  to inspect and manipulate the internal representation if needed.
 int Fixed::getRawBits( void ) const {
-    // std::cout << "getRawBits member function called" << std::endl;
     return this->_numFixedPointValue;
 }
 
@@ -86,4 +76,98 @@ int     Fixed::toInt( void ) const {
 std::ostream & operator<<( std::ostream & o, Fixed const & i ) {
     o << i.toFloat();
     return o;
+}
+
+//----------------- Comparison operators -----------------//
+
+bool    Fixed::operator>( const Fixed &rhs ) const {
+    return this->getRawBits() > rhs.getRawBits();
+}
+
+bool    Fixed::operator<( const Fixed &rhs ) const {
+    return this->getRawBits() < rhs.getRawBits();
+}
+
+bool    Fixed::operator>=( const Fixed &rhs ) const {
+    return this->getRawBits() >= rhs.getRawBits();
+}
+
+bool   Fixed::operator<=( const Fixed &rhs ) const {
+    return this->getRawBits() <= rhs.getRawBits();
+}
+
+bool  Fixed::operator==( const Fixed &rhs ) const {
+    return this->getRawBits() == rhs.getRawBits();
+}
+
+bool    Fixed::operator!=( const Fixed &rhs ) const {
+    return this->getRawBits() != rhs.getRawBits();
+}
+
+//------------------ Arithmetic operators ------------------//
+
+Fixed   Fixed::operator+( const Fixed &rhs ) const {
+    return Fixed( this->toFloat() + rhs.toFloat() );
+}
+
+Fixed   Fixed::operator-( const Fixed &rhs ) const {
+    return Fixed( this->toFloat() - rhs.toFloat() );
+}
+
+Fixed   Fixed::operator*( const Fixed &rhs ) const {
+    return Fixed( this->toFloat() * rhs.toFloat() );
+}
+
+Fixed   Fixed::operator/( const Fixed &rhs ) const {
+    return Fixed( this->toFloat() / rhs.toFloat() );
+}
+
+//------------------ Increment/Decrement operators ------------------//
+
+Fixed&   Fixed::operator++( void ) {
+    ++this->_numFixedPointValue;
+    return *this;
+}
+
+Fixed   Fixed::operator++( int ) {
+    Fixed tmp( *this );
+    tmp._numFixedPointValue = this->_numFixedPointValue++;
+    return tmp;
+}
+
+Fixed& Fixed::operator--( void ) {
+    --this->_numFixedPointValue;
+    return *this;
+}
+
+Fixed Fixed::operator--( int ) {
+    Fixed tmp( *this );
+    tmp._numFixedPointValue = this->_numFixedPointValue--;
+    return tmp;
+}
+
+//----------------------- Max and Min ----------------------//
+
+Fixed& Fixed::min( Fixed &a, Fixed &b ) {
+    if ( a.getRawBits() < b.getRawBits() )
+        return a;
+    return b;
+}
+
+const Fixed& Fixed::min( const Fixed &a, const Fixed &b ) {
+    if ( a.getRawBits() < b.getRawBits() )
+        return a;
+    return b;
+}
+
+Fixed& Fixed::max( Fixed &a, Fixed &b ) {
+    if ( a.getRawBits() > b.getRawBits() )
+        return a;
+    return b;
+}
+
+const Fixed& Fixed::max( const Fixed &a, const Fixed &b ) {
+    if ( a.getRawBits() > b.getRawBits() )
+        return a;
+    return b;
 }
