674fef19037dae4a5eda3d0c8558f8dad52975ea
hboichuk <annabelfeer@gmail.com>
2023-07-27 17:25:53 +0200

    first changes

--- CPP_08/ex01/Span.hpp
+++ CPP_08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: hboichuk <hboichuk@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 16:26:15 by hboichuk          #+#    #+#             */
-/*   Updated: 2023/07/27 17:14:53 by hboichuk         ###   ########.fr       */
+/*   Updated: 2023/07/27 17:24:58 by hboichuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,20 @@
 #include <list>
 
 
+class Span
+{
+	private:
+	
+	public:
+	//ortodox canonical style
+		Span();
+		~Span();
+		Span(Span const &copy);
+		Span &operator=(Span const &copy);
 
+	//getters and setters for access to variables
+	
+};
 
 
 #endif
