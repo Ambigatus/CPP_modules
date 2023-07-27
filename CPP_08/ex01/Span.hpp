d0506ba88fee53f65ab2f22885b1abd4ff693536
hboichuk <annabelfeer@gmail.com>
2023-07-27 17:15:27 +0200

    first files in 01

---
+++ CPP_08/ex01/Span.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hboichuk <hboichuk@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 16:26:15 by hboichuk          #+#    #+#             */
+/*   Updated: 2023/07/27 17:14:53 by hboichuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <exception>
+#include <vector>
+#include <list>
+
+
+
+
+
+#endif
