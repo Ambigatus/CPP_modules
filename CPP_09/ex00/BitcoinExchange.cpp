52a21b9a57dd4fe2d2215f5fb826f2d12971555f
hboichuk <annabelfeer@gmail.com>
2023-08-02 16:20:50 +0200

    first files in 00

--- CPP_09/ex00/BitcoinExchange.cpp
+++ CPP_09/ex00/BitcoinExchange.cpp
@@ -1,19 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hboichuk <hboichuk@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/21 13:03:55 by hboichuk          #+#    #+#             */
-/*   Updated: 2023/08/02 16:19:54 by hboichuk         ###   ########.fr       */
+/*   Created: 2023/08/02 16:13:42 by hboichuk          #+#    #+#             */
+/*   Updated: 2023/08/02 16:15:05 by hboichuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
-
-int	main(int argc, char **argv)
-{
-	
-	return EXIT_SUCCESS;
-}
--- CPP_09/ex00/BitcoinExchange.hpp
+++ CPP_09/ex00/BitcoinExchange.hpp
@@ -1,19 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hboichuk <hboichuk@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/21 13:03:55 by hboichuk          #+#    #+#             */
-/*   Updated: 2023/08/02 16:19:54 by hboichuk         ###   ########.fr       */
+/*   Created: 2023/08/02 16:13:53 by hboichuk          #+#    #+#             */
+/*   Updated: 2023/08/02 16:16:00 by hboichuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "BitcoinExchange.hpp"
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
 
-int	main(int argc, char **argv)
-{
-	
-	return EXIT_SUCCESS;
-}
+
+#endif
