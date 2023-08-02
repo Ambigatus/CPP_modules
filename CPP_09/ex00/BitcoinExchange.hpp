8f3e9c27be54bcc3e113b29b3d43213e163dc0b5
hboichuk <annabelfeer@gmail.com>
2023-08-02 16:59:45 +0200

    first code in 00

--- CPP_09/ex00/BitcoinExchange.hpp
+++ CPP_09/ex00/BitcoinExchange.hpp
@@ -6,12 +6,21 @@
 /*   By: hboichuk <hboichuk@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 16:13:53 by hboichuk          #+#    #+#             */
-/*   Updated: 2023/08/02 16:16:00 by hboichuk         ###   ########.fr       */
+/*   Updated: 2023/08/02 16:59:23 by hboichuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <cmath>
+#include <iomanip>
+
 
 
 #endif
--- CPP_09/ex00/main.cpp
+++ CPP_09/ex00/main.cpp
@@ -6,14 +6,19 @@
 /*   By: hboichuk <hboichuk@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 13:03:55 by hboichuk          #+#    #+#             */
-/*   Updated: 2023/08/02 16:19:54 by hboichuk         ###   ########.fr       */
+/*   Updated: 2023/08/02 16:57:14 by hboichuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
 int	main(int argc, char **argv)
-{
+{	
+	if (argc != 2) 
+	{
+		std::cerr << "You need 2 arguments!" << std::endl;
+		return EXIT_FAILURE;
+	}
 	
 	return EXIT_SUCCESS;
 }
