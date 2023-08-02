a5c3dcab213c3d0c2f6c76d0e92c8f517e6f24ea
hboichuk <annabelfeer@gmail.com>
2023-08-02 15:40:32 +0200

    first files for 09

---
+++ CPP_09/ex00/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hboichuk <hboichuk@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/21 13:03:55 by hboichuk          #+#    #+#             */
+/*   Updated: 2023/08/02 15:37:25 by hboichuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+
+int	main(int argc, char **argv)
+{
+	// std::vector<int> vector;
+	// std::list<int> list;
+	
+	// if (argc != 2)
+	// 	return EXIT_FAILURE;
+	
+	// //first test 
+	// for (int i = 0;i < 10; i++)
+	// 	vector.push_back(i);
+
+	// //second test
+	// for (int y = 0;y < 10; y++)
+	// 	list.push_back(y);
+	
+	// easyfind(vector, std::atoi( argv[1]));
+	// easyfind(list, std::atoi( argv[1]));
+	return EXIT_SUCCESS;
+}
