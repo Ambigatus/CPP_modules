7539aef37c5b209f8d020fdc06ad19ac90c66348
hboichuk <annabelfeer@gmail.com>
2023-08-24 17:56:06 +0200

    add some comments

--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hboichuk <hboichuk@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 13:03:55 by hboichuk          #+#    #+#             */
-/*   Updated: 2023/08/01 16:47:22 by hboichuk         ###   ########.fr       */
+/*   Updated: 2023/08/24 17:55:18 by hboichuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,21 @@
 int	main(void)
 {
 	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
+	mstack.push(5);//adding
+	mstack.push(17); //adding
+	std::cout << mstack.top() << std::endl; //show 17
+	mstack.pop(); //delete 17
+	std::cout << mstack.size() << std::endl; //1
+	mstack.push(3);//adding
+	mstack.push(5);//adding
+	mstack.push(737);//adding
 
-	mstack.push(0);
+	mstack.push(0); //adding
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
+	++it;//just show the iteration
+	--it;//just show the iteration
+	//show us all numbers in the stack without 17, we delete it
 	while (it != ite)
 	{
 		std::cout << *it << std::endl;
